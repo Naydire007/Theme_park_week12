98439a82093ec25025d3130c60f09757915ec949
Stephanie <stephania.l.e@gmail.com>
2021-08-31 10:35:45 +0100

    half

---
+++ src/main/java/attractions/RollerCoaster.java
@@ -0,0 +1,17 @@
+package attractions;
+
+import behaviours.IReviewed;
+import behaviours.ISecurity;
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class RollerCoaster extends Attraction implements ISecurity, IReviewed, ITicketed {
+
+    public RollerCoaster(String name, int rating) {
+        super(name, rating);
+    }
+
+    public boolean isAllowedTo(Visitor visitor){
+        return visitor.getAge() >= 12 && visitor.getHeight() >=145;
+    }
+}
